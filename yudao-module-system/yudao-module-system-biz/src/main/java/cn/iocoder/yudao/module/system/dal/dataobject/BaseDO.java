022c9e0678bc9c0701a2369a30f951e29d6f6615
liubuying <liubuying13@gmail.com>
2025-05-20 09:45:30 +0800

    新增基础baseDO 类

---
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/dal/dataobject/BaseDO.java
@@ -0,0 +1,39 @@
+package cn.iocoder.yudao.module.system.dal.dataobject;
+
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import org.apache.ibatis.type.JdbcType;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * 基础实现类
+ */
+public abstract class BaseDO {
+
+    /**
+     * creatorId
+     */
+    private Long creatorId;
+    /**
+     * 操作人ID
+     */
+    private Long operatorId;
+    /**
+     * 创建时间
+     */
+    private Date gmtCreate;
+    /**
+     * 更新时间
+     */
+    private Date gmtModified;
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Boolean deleted;
+
+}
